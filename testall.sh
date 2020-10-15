16db8e7c3c30f7e28d8a62d5edcad0ac1cce8f54
Hikmat Farhat <hfarhat@ndu.edu.lb>
2020-10-15 17:06:32 +0300

    update

--- testall.sh
+++ testall.sh
@@ -3,6 +3,7 @@ rm -rf tests
 mkdir tests
 cd tests
 cmake .. -DBoost_INCLUDE_DIR=$BOOST_ROOT_1_72_0/include
+make FA-Checker
 ./FA-Checker ../1a.txt ../1a-tests.txt
 ./FA-Checker ../1b.txt ../1b-tests.txt
 ./FA-Checker ../1c.txt ../1c-tests.txt
