06ccc40d6c43f4a9ce1661f61ab9ae56a46f524b
Hikmat Farhat <hfarhat@ndu.edu.lb>
2020-10-15 18:23:49 +0300

    update

--- testall.sh
+++ testall.sh
@@ -4,27 +4,22 @@ mkdir tests
 cd tests
 cmake .. -DBoost_INCLUDE_DIR=$BOOST_ROOT_1_72_0/include
 make FA-Checker
-x=$(./FA-Checker ../1a.txt ../1a-tests.txt)
 echo "Problem 1a"
 echo "----------"
-echo $x
-x=$(./FA-Checker ../1b.txt ../1b-tests.txt)
+./FA-Checker ../1a.txt ../1a-tests.txt |tee -a grade
 echo "Problem 1b"
 echo "----------"
-echo $x
-x=$(./FA-Checker ../1c.txt ../1c-tests.txt)
+./FA-Checker ../1b.txt ../1b-tests.txt|tee -a grade
 echo "Problem 1c"
 echo "----------"
-echo $x
-x=$(./FA-Checker ../1d.txt ../1d-tests.txt)
+./FA-Checker ../1c.txt ../1c-tests.txt |tee -a grade
 echo "Problem 1d"
 echo "----------"
-echo $x
-x=$(./FA-Checker ../1e.txt ../1e-tests.txt)
+./FA-Checker ../1d.txt ../1d-tests.txt |tee -a grade
 echo "Problem 1e"
 echo "----------"
-echo $x
-x=$(./FA-Checker ../2.txt ../2-tests.txt)
+./FA-Checker ../1e.txt ../1e-tests.txt|tee -a grade
 echo "Problem 2"
 echo "----------"
-echo $x
+./FA-Checker ../2.txt ../2-tests.txt|tee -a grade
+cat grade
