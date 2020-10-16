695496f9511db80fbe4b2d9a0535dbd40dbc25c3
Hikmat Farhat <hfarhat@ndu.edu.lb>
2020-10-16 12:07:44 +0300

    update

--- testall.sh
+++ testall.sh
@@ -22,11 +22,7 @@ echo "----------"
 echo "Problem 2"
 echo "----------"
 ./FA-Checker ../2.txt ../2-tests.txt|tee -a grade
-cut -d ' ' -f 5 grade|paste -sd +
-cut -d ' ' -f 7 grade|paste -sd + 
 score=$(cut -d ' ' -f 5 grade|paste -sd +|bc)
 total=$(cut -d ' ' -f 7 grade|paste -sd +|bc)
-/bin/echo -e "\x1B[36mscore\033[0m $score / $total" |tee -a grade
-/bin/echo -e "\x1B[36mscore"
-which echo
+/bin/echo -e "\x1B[36mYour score\033[0m $score / $total" |tee -a grade
 #echo $GITHUB_REPOSITORY|tee -a grade
